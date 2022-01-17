b1a0b4af503200c33155e4d647b74981a4ee6f07
nbabcock <nathan.r.babcock@gmail.com>
2022-01-17 14:14:43 -0600

    Node connectors

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,40 @@
 import React, { useRef } from 'react'
 import ReactDOM from 'react-dom'
 import { Canvas } from '@react-three/fiber'
-import { Mesh } from 'three'
+import { Group, Mesh } from 'three'
 import './index.scss'
 import { OrbitControls } from '@react-three/drei'
+
+/** A connection point for a Block */
+function Node(props: {
+  type?: 'male' | 'female',
+  position?: [number, number, number],
+} = {}) {
+  // This reference will give us direct access to the mesh
+  const mesh = useRef<Mesh>()
+
+  return (
+    <mesh position={props.position} ref={mesh}>
+      <sphereBufferGeometry args={[0.2]} />
+      <meshStandardMaterial color={props.type === 'female' ? 'pink' : 'cornflowerblue'} />
+    </mesh>
+  )
+}
 
 function Block(props: any) {
   // This reference will give us direct access to the mesh
   const mesh = useRef<Mesh>()
 
   return (
-    <mesh {...props} ref={mesh}>
-      <boxBufferGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={'orange'} />
-    </mesh>
+    <group {...props}>
+      <Node type="female" position={[0, -0.5, 0]}/>
+      <Node type="male" position={[0, 0.5, 0]}/>
+
+      <mesh ref={mesh}>
+        <boxBufferGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color={'orange'} />
+      </mesh>
+    </group>
   )
 }
 
@@ -21,7 +42,7 @@ ReactDOM.render(
   <div style={{ width: "100vw", height: "100vh" }}>
     <Canvas>
       <OrbitControls/>
-      <ambientLight />
+      <ambientLight intensity={0.25}/>
       <pointLight position={[10, 10, 10]} />
       <Block position={[-1.2, 0, 0]} />
       <Block position={[1.2, 0, 0]} />
