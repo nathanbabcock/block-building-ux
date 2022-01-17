1332ce7d660f73db2e0009aaa2926f433bd2d8cf
nbabcock <nathan.r.babcock@gmail.com>
2022-01-17 13:56:02 -0600

    drei OrbitControls

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import { Canvas } from '@react-three/fiber'
 import { Mesh } from 'three'
 import './index.scss'
+import { OrbitControls } from '@react-three/drei'
 
 function Block(props: any) {
   // This reference will give us direct access to the mesh
@@ -19,6 +20,7 @@ function Block(props: any) {
 ReactDOM.render(
   <div style={{ width: "100vw", height: "100vh" }}>
     <Canvas>
+      <OrbitControls/>
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
       <Block position={[-1.2, 0, 0]} />
