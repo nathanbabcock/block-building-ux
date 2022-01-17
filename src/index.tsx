794dc694b35249d1735b5789c0a043dfc5181ce9
nbabcock <nathan.r.babcock@gmail.com>
2022-01-17 13:25:20 -0600

    Add stylesheet and background color

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,17 @@
+import React, { useRef } from 'react'
 import ReactDOM from 'react-dom'
-import React, { useRef, useState } from 'react'
-import { Canvas, useFrame } from 'react-three-fiber'
+import { Canvas } from 'react-three-fiber'
 import { Mesh } from 'three'
+import './index.scss'
 
-function Box(props: any) {
+function Block(props: any) {
   // This reference will give us direct access to the mesh
   const mesh = useRef<Mesh>()
 
-  // Set up state for the hovered and active state
-  const [hovered, setHover] = useState(false)
-  const [active, setActive] = useState(false)
-
-  // Rotate mesh every frame, this is outside of React without overhead
-  useFrame(() => {
-    mesh.current!.rotation.x = mesh.current!.rotation.y += 0.01
-  })
-
   return (
-    <mesh
-      {...props}
-      ref={mesh}
-      scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
-      onClick={(event) => setActive(!active)}
-      onPointerOver={(event) => setHover(true)}
-      onPointerOut={(event) => setHover(false)}>
+    <mesh {...props} ref={mesh}>
       <boxBufferGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+      <meshStandardMaterial color={'orange'} />
     </mesh>
   )
 }
@@ -34,8 +20,8 @@ ReactDOM.render(
   <Canvas>
     <ambientLight />
     <pointLight position={[10, 10, 10]} />
-    <Box position={[-1.2, 0, 0]} />
-    <Box position={[1.2, 0, 0]} />
+    <Block position={[-1.2, 0, 0]} />
+    <Block position={[1.2, 0, 0]} />
   </Canvas>,
   document.getElementById('root')
 )
