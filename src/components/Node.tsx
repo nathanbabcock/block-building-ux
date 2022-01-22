649131f1d05db23c352796ca55280b57b1fe76bb
nbabcock <nathan.r.babcock@gmail.com>
2022-01-22 16:57:33 -0600

    Document problems with state confusion

--- src/components/Node.tsx
+++ src/components/Node.tsx
@@ -15,22 +15,38 @@ export default function Node(props: {
   setDraggedMesh: (group?: Group) => void,
 }) {
   const mesh = useRef<Mesh>()
-  const [ dragging, setDragging ] = useState(false)
+  const [ dragging, setDragging ] = useState<boolean>(false)
   const { camera } = useThree()
   const [ plane ] = useState(new Plane())
   const [ lastIntersection ] = useState(new Vector3())
 
+  // https://stackoverflow.com/a/60643670
+  const draggingRef = useRef<boolean>()
+  draggingRef.current = dragging
+  // This may have been a red herring --
+  // The root of the problem is that the hovered node
+  // is NOT the dragged node. There needs to be a way
+  // for the HOVERED node to reset the state of the DRAGGED node.
+  // This likely means extracting the "dragging" state to a single
+  // global shared ancestor (single source of truth).
+  // It might also go along with a full refactor of state management
+  // in this app, using either Context, or Redux, or something else. 
+
   const startDragging = () => {
-    if (dragging) return
+    const isDragging = draggingRef.current
+    if (isDragging) console.log('startDragging called with dragging=', isDragging)
+    if (isDragging) return
     setDragging(true)
     props.setGlobalDragging(true)
     props.setOrbitControlsEnabled(false)
     props.setDraggedMesh(props.parentRef.current)
-    console.log('startDragging')
+    console.log('startDragging', isDragging)
   }
 
   const stopDragging = () => {
-    if (!dragging) return
+    const isDragging = draggingRef.current
+    if (!isDragging) console.log('stopDragging called with dragging=', isDragging)
+    if (!isDragging) return
     setDragging(false)
     props.setGlobalDragging(false)
     props.setOrbitControlsEnabled(true)
@@ -56,7 +72,8 @@ export default function Node(props: {
   }
 
   const onPointerMove = (event: ThreeEvent<MouseEvent>) => {
-    if (!dragging) return
+    const isDragging = draggingRef.current
+    if (!isDragging) return
     const parent = props.parentRef.current as Group
     if (!parent) return
     const ray = event.ray
@@ -65,15 +82,16 @@ export default function Node(props: {
     const delta = intersection.clone().sub(lastIntersection)
     parent.position.add(delta)
     lastIntersection.copy(intersection)
-    console.log('onPointerMove')
+    console.log('onPointerMove', isDragging)
   }
 
   const onHover = (event: ThreeEvent<PointerEvent>) => {
     const curMesh = mesh.current!
     const draggedMesh = props.draggedMesh!
     const parent = curMesh.parent!
+    const isDragging = draggingRef.current
 
-    if (dragging) return
+    if (isDragging) return
     if (!props.globalDragging) return
     if (parent === draggedMesh) return // Can't snap to self
 
@@ -84,7 +102,7 @@ export default function Node(props: {
     const diff = pairWorldPos.sub(curWorldPos)
     const dest = draggedMesh.position.sub(diff)
     draggedMesh.position.copy(dest)
-    console.log('snap')
+    console.log('snap', isDragging)
     stopDragging()
   }
 
